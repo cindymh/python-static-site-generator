1bae8357881da4c6ad55e333f399aff65e55bf7b
cindymh <69194207+cindymh@users.noreply.github.com>
2021-07-11 01:41:43 +1000

    added create directory method in Site class

--- ssg/site.py
+++ ssg/site.py
@@ -4,5 +4,9 @@ from pathlib import Path
 class Site:
 
     def __init__(self, source, dest):
-        self._source = Path(source)
-        self._dest = Path(dest)
+        self.source = Path(source)
+        self.dest = Path(dest)
+
+    def create_dir(self, path):
+        directory = self.dest / path.relative_to(self.source)
+
